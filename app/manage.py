874a8420ba97276e6a0d55b7ba2515a4e9702cc7
Administrator <admin@local.host>
2014-03-22 01:47:49 -0400

    + profile images importing from twitter

--- app/manage.py
+++ app/manage.py
@@ -4,6 +4,11 @@
 from mongoengine import *
 from datetime import datetime
 from bson.dbref import DBRef
+import os
+from twython import Twython
+import urllib
+
+from app import webapp, TWITTER_KEY, TWITTER_SECRET
 from models import *
 import optparse
 
@@ -11,21 +16,26 @@ def process_args(option, opt, value, parser):
     return
 
 def update_db():
-
     connect("quips")
+    users = User.objects()
 
-    recording = Recording.objects()
+    for user in users:
+        twitter = Twython(TWITTER_KEY, TWITTER_SECRET, user.oauthToken, user.oauthTokenSecret)
+        user_info = twitter.show_user(screen_name=user.username)
+        print 'user "%s" profile img: %s' % (user.username, user_info['profile_image_url'])
 
-    #user = User.objects(username='unbuffered')[0]
+        rawId = str(user.id)
+        user_profile_image_filename = user_info['profile_image_url'].split('/')[-1]
 
-    for record in recording:
-        #record.user = user
-        if not record.description:
-            print "> found record with no description: [%s]" % record.description
-            record.description = ""
+        user.profileImage = '/%s/%s' % (rawId, user_profile_image_filename)
+        user.save()
 
-        #record.user = user
-        #record.save()
+        user_profile_image_path = webapp.config['PATH_USER_PROFILE_IMAGE'] + '/%s/' % rawId
+        if not os.path.isdir(user_profile_image_path):
+            os.makedirs(user_profile_image_path)
+        user_profile_image_path = webapp.config['PATH_USER_PROFILE_IMAGE'] + user.profileImage
+        webapp.logger.debug("downloading %s to %s" % (user_info['profile_image_url'], user_profile_image_path))
+        urllib.urlretrieve(user_info['profile_image_url'], user_profile_image_path)
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage='usage: %prog [options]')
